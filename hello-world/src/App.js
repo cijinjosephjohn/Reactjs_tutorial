f174f849d68fa792e1f7c6f0175c225403260f33
Cijin Joseph John <cijinjosephjohn007@gmail.com>
2023-01-29 14:18:00 +0530

    Added two files

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,15 +1,28 @@
+import React,{ Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Greet} from './components/Greet'
+import Greet from './components/Greet'
 import Welcome from './components/welcome'
-
-function App() {
-  return (
-    <div className="App">
-      <Greet></Greet>
-      <Welcome></Welcome>
-    </div>
-  );
+import Message from './components/Message'
+import Counter from './components/Counter';
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        {/* <Greet name="Brue" heroname="Batman">
+          <p>This is a children tag</p></Greet>
+          <Greet name="Tony" heroname="Ironman">
+          <p>This is a children tag</p></Greet>
+          <Greet name="Clark" heroname="Superman">
+          <p>This is a children tag</p></Greet>
+          <Welcome name ="Bruce" heroname ="Batman"></Welcome>
+          <Welcome name ="Tony" heroname="Ironman"></Welcome>
+          <Welcome name = "Clark" heroname="Superman"></Welcome>
+          <Message></Message> */}
+          <Counter></Counter>
+      </div>
+    )
+  }
 }
 
 export default App;
--- hello-world/src/components/Greet.js
+++ hello-world/src/components/Greet.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import App from '../App'
 
-
-export const Greet = () => <h1>Hello all</h1>
-
-//export default Greet;
+//functional component
+const Greet = (props) =>{ 
+// console.log(props)
+return (
+    <div>
+        <h1>Hello {props.name} a.k.a {props.heroname}</h1>
+        {props.children}
+    </div>
+    )
+}
+export default Greet
